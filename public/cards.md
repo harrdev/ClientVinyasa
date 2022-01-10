5aad249cd7ee75609f40da1b4b1cab8df2d027f7
harrdev <jasonharrphx@live.com>
2022-01-10 14:41:40 -0700

    poses displaying based on difficulty > > Co-authored-by: Hannah <hannahrlegros@gmail.com>

---
+++ public/cards.md
@@ -0,0 +1,11 @@
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Body>
+    <Card.Title>{p.englishName}</Card.Title>
+    <Card.Text>
+      Sanskrit Name: {p.sanskritName}
+      Difficulty: {p.difficulty}
+    </Card.Text>
+    <Button variant="primary">Go somewhere</Button>
+  </Card.Body>
+</Card>
--- src/components/CreateRoutine.js
+++ src/components/CreateRoutine.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import Dropdown from 'react-bootstrap/Dropdown'
-
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 const CreateRoutine = (props) => {
     // const { msgAlert, user } = props
     console.log('props in Create Routine', props)
@@ -16,83 +17,65 @@ const CreateRoutine = (props) => {
         setDifficulty(e)
     }
 
-    // const handleSelect = (e) => 
 
-
-    // For loop to pull in poses based on selection from drop down.  Iterate through and attach to a card to display.  Assign an add button, or incorporate into the drag and drop.  Make clickable to open up detail page with description and other things
+    // Maps through all poses based on which difficulty is set in state and renders cards
     const allPoses = props.pose.map((p, i) => {
         if (difficulty === 'beginner') {
             while (p.difficulty === 'beginner') {
                 return (
-                    <li key={i}>
-                        <div id="allPoses">
-                            <div className="poseDiv" id="allPoses">
-                                <div className="poseInfo" id="poseEngName">
-                                    English Name: {p.englishName}
-                                </div>
-                                <div className="poseInfo" id="poseSanName">
-                                    Sanskrit Name: {p.sanskritName}
-                                </div>
-                                <div className="poseInfo" id="poseImgUrl">
-                                    {p.imageUrl}
-                                </div>
-                                <div className="poseInfo" id="poseImgUrl">
-                                    Difficulty: {p.difficulty}
-                                </div>
-                            </div>
-                        </div>
-                    </li>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={p.imageUrl} alt={p.englishName} />
+                        <Card.Body>
+                            <Card.Title>{p.englishName}</Card.Title>
+                            <Card.Text>
+                                {p.sanskritName}
+                            </Card.Text>
+                            <Card.Text>
+                                Difficulty: {p.difficulty}
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
                 )
             }
         }
         else if (difficulty === 'intermediate') {
             while (p.difficulty === 'intermediate' || p.difficulty === 'beginner') {
                 return (
-                    <li key={i}>
-                        <div id="allPoses">
-                            <div className="poseDiv" id="allPoses">
-                                <div className="poseInfo" id="poseEngName">
-                                    English Name: {p.englishName}
-                                </div>
-                                <div className="poseInfo" id="poseSanName">
-                                    Sanskrit Name: {p.sanskritName}
-                                </div>
-                                <div className="poseInfo" id="poseImgUrl">
-                                    {p.imageUrl}
-                                </div>
-                                <div className="poseInfo" id="poseImgUrl">
-                                    Difficulty: {p.difficulty}
-                                </div>
-                            </div>
-                        </div>
-                    </li>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={p.imageUrl} alt={p.englishName} />
+                        <Card.Body>
+                            <Card.Title>{p.englishName}</Card.Title>
+                            <Card.Text>
+                                {p.sanskritName}
+                            </Card.Text>
+                            <Card.Text>
+                                Difficulty: {p.difficulty}
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>
                 )
             }
         }
         else {
             return (
-                <li key={i}>
-                    <div id="allPoses">
-                        <div className="poseDiv" id="allPoses">
-                            <div className="poseInfo" id="poseEngName">
-                                English Name: {p.englishName}
-                            </div>
-                            <div className="poseInfo" id="poseSanName">
-                                Sanskrit Name: {p.sanskritName}
-                            </div>
-                            <div className="poseInfo" id="poseImgUrl">
-                                {p.imageUrl}
-                            </div>
-                            <div className="poseInfo" id="poseImgUrl">
-                                Difficulty: {p.difficulty}
-                            </div>
-                        </div>
-                    </div>
-                </li>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={p.imageUrl} alt={p.englishName} />
+                    <Card.Body>
+                        <Card.Title>{p.englishName}</Card.Title>
+                        <Card.Text>
+                            {p.sanskritName}
+                        </Card.Text>
+                        <Card.Text>
+                            Difficulty: {p.difficulty}
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                </Card>
             )
         }
     })
-
     // Create drag and drop list for selected poses.  Add in remove functionality as well.  Also, a timer in the selected poses section.  If overall timer is longer than cumulitive pose timer, adjust pose timers to equal overall timer
     return (
         <>
