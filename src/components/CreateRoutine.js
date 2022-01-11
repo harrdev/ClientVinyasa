64fc3e4eb899700aaf44d0a423675a6d38f96fb8
hannahlegros <hannahrlegros@gmail.com>
2022-01-11 14:43:21 -0500

    drag and drop workinggit add .git add . > > Co-authored-by: Jason <jasonharrphx@live.com>

--- src/components/CreateRoutine.js
+++ src/components/CreateRoutine.js
@@ -8,131 +8,164 @@ import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 import { addAsana } from '../api/asana'
 
 const CreateRoutine = (props) => {
-    const { msgAlert, user } = props
-    console.log('props in Create Routine', props)
+	const { msgAlert, user } = props
+	console.log('props in Create Routine', props)
 
-    // Define States
-    const [difficulty, setDifficulty] = useState('')
-    const [addPose, setAddPose] = useState([])
+	// Define States
+	const [difficulty, setDifficulty] = useState('')
+	const [addPose, setAddPose] = useState([])
 
-    //***************** Change Handler Function for drop-down difficulty level *****************/
-    const handleChange = (e) => {
-        // console.log('this is e', e)
-        setDifficulty(e)
-    }
+	//***************** Change Handler Function for drop-down difficulty level *****************/
+	const handleChange = (e) => {
+		// console.log('this is e', e)
+		setDifficulty(e)
+	}
 
-    //**************** Add pose click handler to Routine *******************/
-    const addNewPose = (e) => {
-        console.log("This is what's being saved to pose: ", e)
-        setAddPose([...addPose, e])
-        console.log('Spread Operator', addPose)
-    }
+	//**************** Add pose click handler to Routine *******************/
+	const addNewPose = (e) => {
+		console.log("This is what's being saved to pose: ", e)
+		setAddPose([...addPose, e])
+		console.log('Spread Operator', addPose)
+	}
 
-    const displayPose = addPose.map((p, i) => {
+	const displayPose = addPose.map((p, i) => {
+		return (
+			<li className="addedPoses">
+				{p.englishName}
+			</li>
+		)
+	})
+
+	function handleOnDragEnd(result) {
+		if (!result.destination) return;
+
+		const poses = Array.from(addPose);
+		const [reorderedItem] = poses.splice(result.source.index, 1);
+		poses.splice(result.destination.index, 0, reorderedItem);
+
+		setAddPose(poses);
+	}
+
+	//*************** Map loop to iterate through selected difficulty level and display poses ******************/
+	const allPoses = props.pose.map((p, i) => {
+		if (difficulty === 'beginner') {
+			while (p.difficulty === 'beginner') {
+				return (
+					<Card style={{ width: '18rem' }}>
+						<Card.Img variant="top" src={p.imageUrl} alt={p.englishName} />
+						<Card.Body>
+							<Card.Title>{p.englishName}</Card.Title>
+							<Card.Text>
+								{p.sanskritName}
+							</Card.Text>
+							<Card.Text>
+								Difficulty: {p.difficulty}
+							</Card.Text>
+							<Button variant="primary" onClick={() => addNewPose(p)}>Add to Routine</Button>
+							<Link to={`/posedetail/${p._id}`}>
+								<Button variant="primary">Show Details</Button>
+							</Link>
+						</Card.Body>
+					</Card>
+				)
+			}
+		}
+		else if (difficulty === 'intermediate') {
+			while (p.difficulty === 'intermediate' || p.difficulty === 'beginner') {
+				return (
+					<Card style={{ width: '18rem' }}>
+						<Card.Img variant="top" src={p.imageUrl} alt={p.englishName} />
+						<Card.Body>
+							<Card.Title>{p.englishName}</Card.Title>
+							<Card.Text>
+								{p.sanskritName}
+							</Card.Text>
+							<Card.Text>
+								Difficulty: {p.difficulty}
+							</Card.Text>
+							<Button variant="primary" onClick={() => addNewPose(p)}>Add to Routine</Button>
+							<Link to={`/posedetail/${p._id}`}>
+								<Button variant="primary">Show Details</Button>
+							</Link>
+						</Card.Body>
+					</Card>
+				)
+			}
+		}
+		else {
 			return (
-				<li className="addedPoses">
-                    {p.englishName}
-				</li>
-            )
-		// console.log('This is the map: ', test)
-    })
+				<Card style={{ width: '18rem' }}>
+					<Card.Img variant="top" src={p.imageUrl} alt={p.englishName} />
+					<Card.Body>
+						<Card.Title>{p.englishName}</Card.Title>
+						<Card.Text>
+							{p.sanskritName}
+						</Card.Text>
+						<Card.Text>
+							Difficulty: {p.difficulty}
+						</Card.Text>
+						<Button variant="primary" onClick={() => addNewPose(p)}>Add to Routine</Button>
+						<Link to={`/posedetail/${p._id}`}>
+							<Button variant="primary">Show Details</Button>
+						</Link>
+					</Card.Body>
+				</Card>
+			)
+		}
+	})
 
-    // useEffect(() => {
-    //     displayPose()
-    // }, [addPose])
+	// Create drag and drop list for selected poses.  Add in remove functionality as well.  Also, a timer in the selected poses section.  If overall timer is longer than cumulitive pose timer, adjust pose timers to equal overall timer
 
-    //*************** Map loop to iterate through selected difficulty level and display poses ******************/
-    const allPoses = props.pose.map((p, i) => {
-        if (difficulty === 'beginner') {
-            while (p.difficulty === 'beginner') {
-                return (
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={p.imageUrl} alt={p.englishName} />
-                        <Card.Body>
-                            <Card.Title>{p.englishName}</Card.Title>
-                            <Card.Text>
-                                {p.sanskritName}
-                            </Card.Text>
-                            <Card.Text>
-                                Difficulty: {p.difficulty}
-                            </Card.Text>
-                            <Button variant="primary" onClick={() => addNewPose(p)}>Add to Routine</Button>
-                            <Link to={`/posedetail/${p._id}`}>
-                                <Button variant="primary">Show Details</Button>
-                            </Link>
-                        </Card.Body>
-                    </Card>
-                )
-            }
-        }
-        else if (difficulty === 'intermediate') {
-            while (p.difficulty === 'intermediate' || p.difficulty === 'beginner') {
-                return (
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={p.imageUrl} alt={p.englishName} />
-                        <Card.Body>
-                            <Card.Title>{p.englishName}</Card.Title>
-                            <Card.Text>
-                                {p.sanskritName}
-                            </Card.Text>
-                            <Card.Text>
-                                Difficulty: {p.difficulty}
-                            </Card.Text>
-                            <Button variant="primary" onClick={() => addNewPose(p)}>Add to Routine</Button>
-                            <Link to={`/posedetail/${p._id}`}>
-                                <Button variant="primary">Show Details</Button>
-                            </Link>
-                        </Card.Body>
-                    </Card>
-                )
-            }
-        }
-        else {
-            return (
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={p.imageUrl} alt={p.englishName} />
-                    <Card.Body>
-                        <Card.Title>{p.englishName}</Card.Title>
-                        <Card.Text>
-                            {p.sanskritName}
-                        </Card.Text>
-                        <Card.Text>
-                            Difficulty: {p.difficulty}
-                        </Card.Text>
-                        <Button variant="primary" onClick={() => addNewPose(p)}>Add to Routine</Button>
-                        <Link to={`/posedetail/${p._id}`}>
-                            <Button variant="primary">Show Details</Button>
-                        </Link>
-                    </Card.Body>
-                </Card>
-            )
-        }
-    })
-
-    // Create drag and drop list for selected poses.  Add in remove functionality as well.  Also, a timer in the selected poses section.  If overall timer is longer than cumulitive pose timer, adjust pose timers to equal overall timer
-
-    return (
-        <>
-            <div className="routinePage">
-                <div className="asanaCards">
-                    <h2>Create Routine Page</h2>
-                    <DropdownButton id="dropdown-basic-button" title={difficulty} onSelect={handleChange}>
-                        <Dropdown.Item eventKey='beginner'>Beginner</Dropdown.Item>
-                        <Dropdown.Item eventKey='intermediate'>Intermediate</Dropdown.Item>
-                        <Dropdown.Item eventKey='advanced'>Advanced</Dropdown.Item>
-                    </DropdownButton>
-                    <ul>
-                        {allPoses}
-                    </ul>
-                </div>
-                <div className="buildRoutine">
+	return (
+		<>
+			<div className="routinePage">
+				<div className="asanaCards">
+					<h2>Create Routine Page</h2>
+					<DropdownButton id="dropdown-basic-button" title={difficulty} onSelect={handleChange}>
+						<Dropdown.Item eventKey='beginner'>Beginner</Dropdown.Item>
+						<Dropdown.Item eventKey='intermediate'>Intermediate</Dropdown.Item>
+						<Dropdown.Item eventKey='advanced'>Advanced</Dropdown.Item>
+					</DropdownButton>
+					<ul>
+						{allPoses}
+					</ul>
+				</div>
+				<DragDropContext onDragEnd={handleOnDragEnd}>
+					<Droppable droppableId="addToPractice">
+						{(provided) => (
+							<ul className="practice" {...provided.droppableProps} ref={provided.innerRef}>
+								{addPose.map(({ englishName, sanskritName, imageUrl }, index) => {
+									return (
+										<Draggable key={englishName} draggableId={englishName} index={index}>
+											{(provided) => (
+												<li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+													<div className="practiceImage">
+														<img src={imageUrl} alt={`${englishName}} Thumb`} />
+													</div>
+													<p>
+														{englishName}
+													</p>
+													<p>
+														{sanskritName}
+													</p>
+												</li>
+											)}
+										</Draggable>
+									);
+								})}
+								{provided.placeholder}
+							</ul>
+						)}
+					</Droppable>
+				</DragDropContext>
+				{/* <div className="buildRoutine">
                     <ul className="buildPractice">
                         {displayPose}
                     </ul>
-                </div>
-            </div>
-        </>
-    )
+                </div> */}
+			</div>
+		</>
+	)
 }
 
 export default CreateRoutine
