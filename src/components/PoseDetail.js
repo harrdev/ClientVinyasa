f30823fa0616278edbd99f66387f6e5ad347f001
harrdev <jasonharrphx@live.com>
2022-01-11 08:34:19 -0700

    details page pulling in data > > Co-authored-by: Hannah <hannahrlegros@gmail.com>

--- src/components/PoseDetail.js
+++ src/components/PoseDetail.js
@@ -1,6 +1,4 @@
 import { useParams } from "react-router";
-import { useState, useEffect } from 'react'
-import { getAsanas } from "../api/asana";
 
 const PoseDetail = (props) => {
     // UseParams pulling in URL ID to match to pose props to match up pose data
@@ -12,13 +10,11 @@ const PoseDetail = (props) => {
     return (
         <>
             <div>
-            <h2>Pose Detail Page</h2>
-            <h2>Pose Selected: {poseContent.englishName}</h2>
-
+                <h2>Pose Detail Page</h2>
+                <h2>Pose Selected: {poseContent.englishName}</h2>
             </div>
         </>
     )
 }
-
 
 export default PoseDetail
